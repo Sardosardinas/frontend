6fe680d7a6c3dbace55ee77f4889073fa0e17c60
Felix Ruano <felixruano.91@gmail.com>
2019-02-08 16:09:57 -0600

    SpoOoOoky!

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Main from './components/Main'
+
+class App extends Component {
+  render() {
+    return (
+      <Main>
+        <h1>
+          Hello World!
+        </h1>
+      </Main>
+    );
+  }
+}
+
+export default App;
